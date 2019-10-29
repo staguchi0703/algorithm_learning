3f4d71d158556f1ba1aa42887e8a8073166d9fc8
staguchi0703 <51268302+staguchi0703@users.noreply.github.com>
2019-10-30 02:27:09 +0900

    Update my_binary_tree.py

--- my_binary_tree.py
+++ my_binary_tree.py
@@ -18,10 +18,9 @@ class BST:
         temp_node = self.root
         if self.root == None:
             self.root = Node(data)
-            return
 
         else:
-            while True:
+            while temp_node:
                 temp_node_val = temp_node.data
                 if data < temp_node_val:
                     if temp_node.left is None:
@@ -41,21 +40,40 @@ class BST:
                 else:
                     temp_node.data = data
                     return
-    
 
-ins = BST([24,12,35,14,5,16,7])
+    ###########################
+    #
+    #  Tree traversal
+    #
+    ###############################
+    def preoder_traversal(self, node, res):
+        if node == None:
+            return res
+        
+        print('stack', node.data)
+        res.append(node.data)
+
+        # 前順序で左部分木
+        self.preoder_traversal(node.left, res)
+        # 前順序で右部分木
+        self.preoder_traversal(node.right, res)
+
+
+ins = BST([24,12,3, 5,14,5,16,7,8, 11, 99])
 ins_root = ins.root
 
-def viewer(ins_root):
+def left_wing_viewer(ins_root):
     try:
         temp_data = ins_root.data
         print(temp_data)
     except AttributeError:
-        
         return
 
-    return viewer(ins_root.left)
+    return left_wing_viewer(ins_root.left)
+
+
 
 # TODO 探索の方法を確認して実装する
 
-viewer(ins_root)
+res = ins.preoder_traversal(ins.root, [])
+print(res)
