e0371ca3f31d2b21ddf86842625678c9b24bb2ad
sho Taguchi <s.taguchi0703@gmail.com>
2019-11-08 04:08:12 +0900

    insertを要修正　for loop中のinsertする場所がおかしい

--- my_binary_tree.py
+++ my_binary_tree.py
@@ -12,6 +12,7 @@ class BST:
         self.root = None # 今後の処理の基準にするために空のrootを作成する
 
         for inserted_node_data in arr: #リストに格納されているノードの値を順次挿入する処理
+            print('....')
             print('try inserting ', inserted_node_data)
             self.insert(inserted_node_data)
 
@@ -20,9 +21,45 @@ class BST:
             print('Root node is ....')
             self.root = Node(data) # Node()インスタンスを生成して代入する
             print(self.root.data)
-
+            
         else:
-            _insert(self.root, data) # クラス外にに定義した再帰関数 。ノードの枝を進んでいく
+            flag = True
+            queue = [self.root]
+            level = 1
+            while flag:
+                level += 1
+                temp_list, queue = queue, []
+
+                for i, node in enumerate(temp_list):
+                    
+                    if node.left is not None:
+                        queue.append(node.left)
+                    if node.right is not None:
+                        queue.append(node.right)
+
+                    if node.left is None:
+                        node.left = Node(data)
+                        queue.append(node.left)
+                        print('inseting level {}'.format(level))
+                        print('inserting {}th block'.format(i))
+                        flag = False
+
+                    elif node.right is None:
+                        node.right = Node(data)
+                        queue.append(node.right)
+                        print('inseting level {}'.format(level))
+                        print('inserting {}th block'.format(i))
+                        flag = False
+                    else:
+                        if not any(queue):
+                            temp_list[0].left = Node(data)
+                            print('inseting level {}'.format(level))
+                            print('inserting {}th block'.format(0))
+                            flag = False
+                # insertを要修正　for loop中のinsertする場所がおかしい
+
+
+
 
     ##########################
     #  Tree traversal
@@ -94,6 +131,9 @@ class BST:
         self.level_order_traversal(queue, res)
 
         return res
+
+
+
 # 二分木の問題
 class BT_method(BST):
     def __init__(self, arr):
@@ -127,65 +167,111 @@ class BT_method(BST):
         return False
 
 
-    def size(self, node, cnt):
-        if node:
-            cnt += 1
-        # nodeの枝がNoneになったときの処理
-        return self.size(node.left, cnt) + self.size(node.right, self.size(node.left, cnt))
-
-        
-
-# BSTクラス内で使用する再帰関数
-# 与えられたの順にノードに追加していく
-def _insert(temp_node, data): #とあるノードに対して左枝と右枝へ条件分岐しながら再帰で進んで、treeの末端に到達したらノードを生成する
-    if temp_node.left != None:
-        if temp_node.right != None:
-            _insert(temp_node.left, data)
+    def size(self, node):
+        if node is None: #nodeがNoneのところで数え上げを終了
+            return 0 #0を返せば数え上げされない
         else:
-            print('inserting to right')
-            temp_node.right = Node(data)
-    else:
-        print('inserting to left')
-        temp_node.left = Node(data)
+            left_cnt = self.size(node.left)
+            right_cnt = self.size(node.right)
+
+            return 1 + left_cnt + right_cnt #自分（not None）が１と左右木中の数（仮想の探索を再帰関数で実現）
 
 
-ins = BST(range(1,16))
+    def hight(self, level=0):
+        flag = True
+        queue = [self.root]
+
+        while flag:
+            level += 1
+            temp_list, queue = queue, []
+            for node in temp_list:
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
 
 
-# TODO 探索の方法を確認して実装する
-print('--------------------------')
-print('start preoder traversal')
-print(ins.preoder_traversal(ins.root, []))
+            flag = any(queue)
 
-print('--------------------------')
-print('start inoder traversal')
-print(ins.inoder_traversal(ins.root, []))
+        return level
 
-print('--------------------------')
-print('start postoder traversal')
-print(ins.postorder_traversal(ins.root, []))
 
-print('--------------------------')
-print('start level order traversal')
-print(ins.level_order_traversal([]))
 
-# 二分木の問題6.6.7
-print('=====================================')
 
-ins2 = BT_method(range(1,16))
-print('--------------------------')
-print('find max')
-print(ins2.max_in_binary_tree(ins2.root, 0))
-print('--------------------------')
-print('find value')
-print('looking for 7', ins2.find_val(ins2.root, 7))
-print('looking for 17', ins2.find_val(ins2.root, 17))
+    # def highest_node(self, queue=[]):
+
+    #     if queue == [] :
+    #         queue.append(self.root)
+
+    #     else:
+    #         # このlevelのノード は引数のqueueだからforで回す
+    #         temp_list , queue = queue, []
+    #         not_none_cnt = 0
+
+    #         for item in temp_list:
+    #             if item.left is not None:
+    #                 queue.append(item.left)
+    #                 not_none_cnt += 1
+
+    #             if item.right is not None:
+    #                 queue.append(item.right)
+    #                 not_none_cnt += 1
+            
+    #         if not_none_cnt == 0:
+    #             return
+            
+    #     self.highest_node(queue)
+    #     return 
+
+# ins = BST(range(1,16))
+
+
+# # TODO 探索の方法を確認して実装する
+# print('--------------------------')
+# print('start preoder traversal')
+# print(ins.preoder_traversal(ins.root, []))
+
+# print('--------------------------')
+# print('start inoder traversal')
+# print(ins.inoder_traversal(ins.root, []))
+
+# print('--------------------------')
+# print('start postoder traversal')
+# print(ins.postorder_traversal(ins.root, []))
+
+# print('--------------------------')
+# print('start level order traversal')
+# print(ins.level_order_traversal([]))
+
+# # 二分木の問題6.6.7
+# print('=====================================')
+
+ins2 = BT_method(range(1,7))
+# print('--------------------------')
+# print('find max')
+# print(ins2.max_in_binary_tree(ins2.root, 0))
+# print('--------------------------')
+# print('find value')
+# print('looking for 7', ins2.find_val(ins2.root, 7))
+# print('looking for 17', ins2.find_val(ins2.root, 17))
 
 
 #6-6 search size
+# print('--------------------------')
+# print('detect node size')
+# print(ins2.size(ins2.root))
+
+#6-10 search hight
 print('--------------------------')
-print('ditect node size')
-print(ins2.size(ins2.root, 0))
+print('detect node hight')
+print(ins2.hight())
+
+#6-12 highest node
+# print('--------------------------')
+# print('detect highest node')
+# print(ins2.highest_node())
+
+
 
 # 次は6.7N分木
 # 次は6.112分探索木
