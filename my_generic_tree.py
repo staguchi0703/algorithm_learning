4cdfca40ad6b627f278166a06732bc10ed2fbb02
sho Taguchi <s.taguchi0703@gmail.com>
2019-11-14 02:30:58 +0900

    generictree_thread_binary_tree

--- my_generic_tree.py
+++ my_generic_tree.py
@@ -1,30 +1,49 @@
-class Generic_tree_Node:
-    def __init__(self, data):
-        self.data = data
-        self.firstchild = None
-        self.firstchild_flag = 1
-        self.nextsibling = None
-        self.nextsibling_flag = 1
+# generic tree
+# --> threaded binary tree
 
-class Generic_tree:
+
+class Threaded_binary_tree_Node:
+    def __init__(self, data):
+        # inorder thread binary tree
+        self.data = data
+        self.firstchild_flag = 0    #thread
+        self.firstchild = None
+        self.nextsibling_flag = 0   #thread
+        self.nextsibling = None
+
+
+class Threaded_binary_tree:
     def __init__(self, node_arr):
         self.root = None
 
+        # dummy nodeの追加
+        self.dummy_node = Threaded_binary_tree_Node('Null')
+        self.dummy_node.nextsibling = self.root
+        self.dummy_node.nextsibling_flag = 1
+        self.dummy_node.firstchild = self.dummy_node
+        self.dummy_node.firstchild_flag = 1
+        
         for i, family in enumerate(node_arr):
             self.insert(i, family)
 
 
     def insert(self, i, family):
         if self.root is None:
-            self.root = Generic_tree_Node(family[0][0])
+            self.root = Threaded_binary_tree_Node(family[0][0])
+            self.root.firstchild = Threaded_binary_tree_Node(family[0][1])
+            self.root.firstchild_flag = 1
 
 
-            temp_node = self.root
+            temp_node = self.root.firstchild
             siblings = [contents[1] for contents in family]
 
             for sibling in siblings:
-                temp_node.nextsibling = Generic_tree_Node(sibling)
+                temp_node.nextsibling = Threaded_binary_tree_Node(sibling)
+                temp_node.nextsibling_flag = 1
                 temp_node = temp_node.nextsibling
+            else:
+                temp_node = self.dummy_node
+
 
         else:
             if i >=1:
@@ -43,47 +62,54 @@ class Generic_tree:
                         if node.firstchild is not None:
                             next_queue.append(node.firstchild)
                         if node.data == pairent:
-                            node.firstchild = Generic_tree_Node(child)
-
+                            node.firstchild = Threaded_binary_tree_Node(child)
+                            node.firstchild_flag = 1
                             temp_node = node.firstchild
                             siblings = [contents[1] for contents in family]
 
                             for sibling in siblings:
-                                temp_node.nextsibling = Generic_tree_Node(sibling)
+                                temp_node.nextsibling = Threaded_binary_tree_Node(sibling)
                                 temp_node = temp_node.nextsibling
 
-                    flag = any(next_queue)
+                    if len(next_queue) >= 1:
+                        flag = None
+                
+                # temp_queue[-1].firstchild = self.dummy_node
 
-    
-    def thread_binary_tree(self, node, res= []):
+    def inorder_successor(self, node):
+        """ 
+        とあるnodeのinorder traverse時の後nodeを探す
+        """
+        if node.nextsibling_flag == 0:
+            return node.nextsibling
+
+        else: # node.nextsibling_flag == 1: 
+            position = node.nextsibling
+            while position.firstchild_flag == 1:
+                position = position.firstchild
+
+            return position
+
+
+
+    def traverse(self, node):
 
         # 二分木化してしまっているので、レベルは右にいった高さで表現する必要あり
         # これを上手く扱う方法としてスレッド二分木を使用する
 
-        # dummy nodeの追加
-        self.dummy_node = Generic_tree_Node('Null')
-        self.dummy_node.nextsibling = self.root
-        # https://yottagin.com/?p=3423
-        self.dummy_node.firstchild = self.dummy_node.firstchild
-
         # 走査の終了条件
-        if node is None:
-            return
-        
-        res.append(node.data)
+        flag = True
+        while node:
+            temp_node = self.inorder_successor(node) 
 
-        if node.nextsibling is not None:
-            self.thread_binary_tree(node.nextsibling, res)
-        else:
-            return
+            if temp_node:
+                print(temp_node.data, temp_node.nextsibling_flag, temp_node.firstchild_flag)
 
-        if node.firstchild is None:
-            self.thread_binary_tree(node.nextsibling, res)
-            self.thread_binary_tree(node.firstchild, res)    
-        else:
-            return
+            node = temp_node
 
-        return res
+        return
+
+
 
 
             
@@ -97,6 +123,7 @@ node_arr =[
             [[11, 16], [11, 17]]
 ]
 
-ins_generic = Generic_tree(node_arr)
+ins_generic = Threaded_binary_tree(node_arr)
 
-print(ins_generic.thread_binary_tree(ins_generic.root))
+print(ins_generic.root.data)
+ins_generic.traverse(ins_generic.root.firstchild)
